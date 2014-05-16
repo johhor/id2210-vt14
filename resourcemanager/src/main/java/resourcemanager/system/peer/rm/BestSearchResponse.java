666e9956876319deda3c22cdf2d1002950190ed0
linus91 <linus.pettersson@charismait.se>
2014-05-16 11:34:28 +0200

    added best search class

---
+++ resourcemanager/src/main/java/resourcemanager/system/peer/rm/BestSearchResponse.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package resourcemanager.system.peer.rm;
+
+/**
+ *
+ * @author Linus
+ */
+public class BestSearchResponse {
+    private final int numCpus;
+    private final int amountMemInMb;
+    private final int time;
+    private final boolean isCpuMsg;
+    
+    SearchResourceMsg.Response bestResponse;
+    
+    public BestSearchResponse(int numCpus, int amountMemInMb, int time, boolean isCpuMsg, SearchResourceMsg.Response bestResponse) {
+        this.numCpus = numCpus;
+        this.amountMemInMb = amountMemInMb;
+        this.time =time;
+        this.isCpuMsg = isCpuMsg;
+        this.bestResponse = bestResponse;
+    }
+    
+    public void replaceBestResponse(SearchResourceMsg.Response response) {
+        bestResponse = response;
+    }
+
+    public SearchResourceMsg.Response getBestResponse() {
+        return bestResponse;
+    }
+
+    public boolean isCpuMsg() {
+        return isCpuMsg;
+    }
+    
+    public int getNumCpus() {
+        return numCpus;
+    }
+    public int getAmountMemInMb() {
+        return amountMemInMb;
+    }
+    public int getTime() {
+        return time;
+    }
+}
