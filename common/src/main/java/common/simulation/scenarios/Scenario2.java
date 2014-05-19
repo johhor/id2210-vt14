9828b8e0d444dec5eec2f115152b5ccb239a9ecd
linus91 <linus.pettersson@charismait.se>
2014-05-19 18:05:08 +0200

    Update Senario2 + run

--- common/src/main/java/common/simulation/scenarios/Scenario2.java
+++ common/src/main/java/common/simulation/scenarios/Scenario2.java
@@ -6,21 +6,17 @@ import se.sics.kompics.p2p.experiment.dsl.SimulationScenario;
 @SuppressWarnings("serial")
 public class Scenario2 extends Scenario {
     static final int NUM_REQUESTING_PROCESSES = 100;
-    static final int NUM_JOINING_PROCESSES = 10;
-    static final int NUM_FAILING_PROCESSES = 10;
     
 	private static SimulationScenario scenario = new SimulationScenario() {{
             
-            for (int i=0; i<NUM_JOINING_PROCESSES; i++){
 		SimulationScenario.StochasticProcess process0 = new SimulationScenario.StochasticProcess() {{
 			eventInterArrivalTime(constant(1000));
-			raise(3, Operations.peerJoin(), 
+			raise(NUM_REQUESTING_PROCESSES, Operations.peerJoin(), 
                                 uniform(0, Integer.MAX_VALUE), 
                                 constant(8), constant(12000)
                              );
 		}};
                 process0.start();
-                }
             
             for (int i=0; i<NUM_REQUESTING_PROCESSES; i++){
                 SimulationScenario.StochasticProcess process1 = new SimulationScenario.StochasticProcess() {{
@@ -31,16 +27,15 @@ public class Scenario2 extends Scenario {
                                 constant(1000*60*1) // 1 minute
                                 );
 		}};
-                process1.start();
+                process1.startAfterTerminationOf(2000, process0);
             }
-            for (int i=0; i<NUM_FAILING_PROCESSES; i++) {
+
             SimulationScenario.StochasticProcess failPeersProcess = new SimulationScenario.StochasticProcess() {{
 			eventInterArrivalTime(constant(100));
 			raise(1, Operations.peerFail, 
                                 uniform(0, Integer.MAX_VALUE));
             }};
             failPeersProcess.start();
-            }
                 
                 
                 SimulationScenario.StochasticProcess process1 = new SimulationScenario.StochasticProcess() {{
@@ -51,14 +46,14 @@ public class Scenario2 extends Scenario {
                                 constant(1000*60*1) // 1 minute
                                 );
 		}};
-                process1.start();
+                process1.startAfterTerminationOf(2000, process0);
                 
 		SimulationScenario.StochasticProcess terminateProcess = new SimulationScenario.StochasticProcess() {{
 			eventInterArrivalTime(constant(100));
 			raise(1, Operations.terminate);
 		}};
 
-                terminateProcess.startAfterTerminationOf(100*1000, process1);
+                //terminateProcess.startAfterTerminationOf(100*1000, process1);
 	}};
 
 	// -------------------------------------------------------------------
--- resourcemanager/src/main/java/main/Main.java
+++ resourcemanager/src/main/java/main/Main.java
@@ -4,6 +4,7 @@ import simulator.core.DataCenterSimulationMain;
 import common.configuration.Configuration;
 import common.simulation.scenarios.Scenario;
 import common.simulation.scenarios.Scenario1;
+import common.simulation.scenarios.Scenario2;
 
 public class Main {
 
@@ -12,7 +13,7 @@ public class Main {
         long seed = System.currentTimeMillis();
         Configuration configuration = new Configuration(seed);
 
-        Scenario scenario = new Scenario1();
+        Scenario scenario = new Scenario2();
         scenario.setSeed(seed);
         scenario.getScenario().simulate(DataCenterSimulationMain.class);
     }
