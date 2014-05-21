42879d0f25688007f42d283f0f8692aaa4a0585f
linus91 <linus.pettersson@charismait.se>
2014-05-21 10:03:25 +0200

    Update Scenario2

--- common/src/main/java/common/simulation/scenarios/Scenario2.java
+++ common/src/main/java/common/simulation/scenarios/Scenario2.java
@@ -5,46 +5,53 @@ import se.sics.kompics.p2p.experiment.dsl.SimulationScenario;
 
 @SuppressWarnings("serial")
 public class Scenario2 extends Scenario {
-    static final int NUM_REQUESTING_PROCESSES = 100;
-    
-	private static SimulationScenario scenario = new SimulationScenario() {{
-            
-		SimulationScenario.StochasticProcess process0 = new SimulationScenario.StochasticProcess() {{
-			eventInterArrivalTime(constant(1000));
-			raise(NUM_REQUESTING_PROCESSES, Operations.peerJoin(), 
-                                uniform(0, Integer.MAX_VALUE), 
-                                constant(8), constant(12000)
-                             );
-		}};
-                process0.start();
-                SimulationScenario.StochasticProcess process1 = null;
-            for (int i=0; i<NUM_REQUESTING_PROCESSES; i++){
-                process1 = new SimulationScenario.StochasticProcess() {{
-			eventInterArrivalTime(constant(100));
-			raise(100, Operations.requestResources(), 
-                                uniform(0, Integer.MAX_VALUE),
-                                constant(2), constant(2000),
-                                constant(1000*60*1) // 1 minute
-                                );
-		}};
-                process1.startAfterTerminationOf(2000, process0);
-            }
+	static final int NUM_REQUESTING_PROCESSES = 5;
 
-            SimulationScenario.StochasticProcess failPeersProcess = new SimulationScenario.StochasticProcess() {{
-			eventInterArrivalTime(constant(100));
-			raise(1, Operations.peerFail, 
-                                uniform(0, Integer.MAX_VALUE));
-            }};
-            failPeersProcess.start();
-                
-                
-		SimulationScenario.StochasticProcess terminateProcess = new SimulationScenario.StochasticProcess() {{
-			eventInterArrivalTime(constant(100));
-			raise(1, Operations.terminate);
-		}};
-				if (process1 != null)
-                terminateProcess.startAfterTerminationOf(100*10000*2, process1);
-	}};
+	private static SimulationScenario scenario = new SimulationScenario() {
+		{
+
+			SimulationScenario.StochasticProcess process0 = new SimulationScenario.StochasticProcess() {
+				{
+					eventInterArrivalTime(constant(1000));
+					raise(NUM_REQUESTING_PROCESSES, Operations.peerJoin(),
+							uniform(0, Integer.MAX_VALUE), constant(8),
+							constant(12000));
+				}
+			};
+
+			SimulationScenario.StochasticProcess process1 = null;
+
+			process1 = new SimulationScenario.StochasticProcess() {
+				{
+					eventInterArrivalTime(constant(100));
+					raise(100, Operations.requestResources(),
+							uniform(0, Integer.MAX_VALUE), constant(2),
+							constant(2000), constant(10 * 60 * 1) // 1
+																	// minute
+					);
+				}
+			};
+
+			SimulationScenario.StochasticProcess failPeersProcess = new SimulationScenario.StochasticProcess() {
+				{
+					eventInterArrivalTime(constant(100));
+					raise(1, Operations.peerFail, uniform(0, Integer.MAX_VALUE));
+				}
+			};
+			// failPeersProcess.start();
+
+			SimulationScenario.StochasticProcess terminateProcess = new SimulationScenario.StochasticProcess() {
+				{
+					eventInterArrivalTime(constant(100));
+					raise(1, Operations.terminate);
+				}
+			};
+			
+			process0.start();
+			process1.startAfterTerminationOf(2000, process0);
+			terminateProcess.startAfterTerminationOf(2000, process1);
+		}
+	};
 
 	// -------------------------------------------------------------------
 	public Scenario2() {
