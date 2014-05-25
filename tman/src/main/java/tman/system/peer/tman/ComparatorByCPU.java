50246ac208ffee6cf0182b3787dbfc72bbad28e6
linus91 <linus.pettersson@charismait.se>
2014-05-25 08:24:05 +0200

    Bugfix Comparator TMan

--- tman/src/main/java/tman/system/peer/tman/ComparatorByCPU.java
+++ tman/src/main/java/tman/system/peer/tman/ComparatorByCPU.java
@@ -16,21 +16,27 @@ public class ComparatorByCPU extends QueueLengthComparer implements Comparator<P
     public ComparatorByCPU(PeerDescriptor self) {
         super(self);
     }
+    
     @Override
     public int compare(PeerDescriptor o1, PeerDescriptor o2) {
-        if (o1.getAvailableResources().getNumFreeCpus() == o2.getAvailableResources().getNumFreeCpus())
+        if (o1.getAvailableResources().getNumFreeCpus() == o2.getAvailableResources().getNumFreeCpus() && o1.getAvailableResources().getQueueLength() == o2.getAvailableResources().getQueueLength() )
             return 0;
-        if (o1.getAvailableResources().getQueueLength()>0 && o2.getAvailableResources().getQueueLength()>0) {
+        if (o1.getAvailableResources().getQueueLength()>0 || o2.getAvailableResources().getQueueLength()>0) {
             return super.queueCompare(o1, o2);
-        } else 
-            if (o1.getAvailableResources().getNumFreeCpus() < self.getAvailableResources().getNumFreeCpus() && o2.getAvailableResources().getNumFreeCpus() > self.getAvailableResources().getNumFreeCpus()) {
+        } 
+        else if (o1.getAvailableResources().getNumFreeCpus() == o2.getAvailableResources().getNumFreeCpus()) {
+        	return 0;
+        } else if (o1.getAvailableResources().getNumFreeCpus() <= self.getAvailableResources().getNumFreeCpus() && o2.getAvailableResources().getNumFreeCpus() > self.getAvailableResources().getNumFreeCpus()) {
             return 1;
-        } else if (o2.getAvailableResources().getNumFreeCpus() < self.getAvailableResources().getNumFreeCpus() && o1.getAvailableResources().getNumFreeCpus() > self.getAvailableResources().getNumFreeCpus()) {
+        } else if (o2.getAvailableResources().getNumFreeCpus() <= self.getAvailableResources().getNumFreeCpus() && o1.getAvailableResources().getNumFreeCpus() > self.getAvailableResources().getNumFreeCpus()) {
             return -1;
-        } else if (Math.abs(o1.getAvailableResources().getNumFreeCpus() - self.getAvailableResources().getNumFreeCpus()) < Math.abs(o2.getAvailableResources().getNumFreeCpus() - self.getAvailableResources().getNumFreeCpus())) {
+        } 
+        else if (Math.abs(o1.getAvailableResources().getNumFreeCpus() - self.getAvailableResources().getNumFreeCpus()) == Math.abs(o2.getAvailableResources().getNumFreeCpus() - self.getAvailableResources().getNumFreeCpus())) {
+        	return 0;
+        }
+        else if (Math.abs(o1.getAvailableResources().getNumFreeCpus() - self.getAvailableResources().getNumFreeCpus()) < Math.abs(o2.getAvailableResources().getNumFreeCpus() - self.getAvailableResources().getNumFreeCpus())) {
             return -1;
         }
         return 1;
     }
-    
 }
