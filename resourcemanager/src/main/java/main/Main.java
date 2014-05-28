a67b2af138f9afa7488def2788045658d874ed5b
linus91 <linus.pettersson@charismait.se>
2014-05-28 16:06:17 +0200

    Change scenario

--- resourcemanager/src/main/java/main/Main.java
+++ resourcemanager/src/main/java/main/Main.java
@@ -6,6 +6,7 @@ import common.simulation.scenarios.Scenario;
 import common.simulation.scenarios.Scenario1;
 import common.simulation.scenarios.Scenario2;
 import common.simulation.scenarios.BatchScenario1;
+import common.simulation.scenarios.BatchScenario2;
 
 public class Main {
 
@@ -14,7 +15,7 @@ public class Main {
         long seed = System.currentTimeMillis();
         Configuration configuration = new Configuration(seed);
 
-        Scenario scenario = new BatchScenario1();
+        Scenario scenario = new BatchScenario2();
         scenario.setSeed(seed);
         scenario.getScenario().simulate(DataCenterSimulationMain.class);
     }
